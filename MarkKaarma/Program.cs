d2264472e072809e223020e4d2bc43cc1238b3bc
MarkKaarma <mark.kaarma@gmail.com>
2020-04-09 12:44:36 +0300

    Create Hello App

--- MarkKaarma/Program.cs
+++ MarkKaarma/Program.cs
@@ -10,6 +10,11 @@ namespace MarkKaarma
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please, enter your name: ");
+            string name = Console.ReadLine();
+            
+            Console.WriteLine($"Hello, {name}!");
+            Console.ReadLine();
         }
     }
 }
